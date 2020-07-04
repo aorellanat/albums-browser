7abd959419b3125d7d4fd25f47b0b4643d4ee512
Adriana <adrianaorellana34@gmail.com>
2020-07-04 16:50:28 -0400

    develop: album component and home view added

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card class="mb-2">
+    <v-row no-gutters justify="end">
+      <v-col cols="6" sm="6" md="6">
+        <v-img :src="urlImageCover" class="image"></v-img>
+      </v-col>
+      <v-col cols="6" sm="6" md="6">
+      <v-card-title class="mt-1 ml-5">Album: {{album}}</v-card-title>
+      <v-card-subtitle class="mt-1 ml-5">{{artist}}</v-card-subtitle>
+      <v-card-text class="ml-5">Price: {{price}}</v-card-text>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    album: {
+      type: String,
+      required: true
+    },
+    artist: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    urlImageCover: {
+      type: String,
+      default: 'https://resistencia.es/wp-content/uploads/2018/10/music.png'
+    }
+  },
+}
+</script>
+
+<style scoped>
+.image {
+  height: 100%;
+  width: 40vw;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <h1 id="title" class="text-center my-5">iTunes Artists</h1>
+    <p id="instruction-text">Please enter artist name to search information about his albums:</p>
+    <v-row justify="center">
+      <v-col cols="8" sm="6" md="6">
+        <v-text-field id="artist-name" outlined label="Artist"></v-text-field>
+      </v-col>
+      <v-col cols="3" sm="2" md="2">
+        <v-btn id="search-button" class="mt-1" color="pink darken-3" dark large>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
